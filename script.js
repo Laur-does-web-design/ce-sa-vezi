70a77d8356e80a77c982f68df7f8c90577c85c39
Laur-does-web-design <63873233+Laur-does-web-design@users.noreply.github.com>
2020-04-19 15:14:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("nav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+}
+
+function dark() {
+    document.body.classList.toggle("dark")
+}
